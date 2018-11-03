69ba514d73c024a2f8689418af494743f592974b
Royce Birnbaum <royce.birnbaum@gmail.com>
2018-11-03 13:30:32 -0700

    push

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import './home.css';
 class App extends Component {
-  constructor (props) {
-    super(props);
-  }
   render () {
     return (
       <div className="main">
